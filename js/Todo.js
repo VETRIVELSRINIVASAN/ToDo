020f349338bb300def33664f2c3a9da06c23347c
VETRIVELSRINIVASAN <vetrivel.s@e5.ai>
2021-11-08 22:29:31 +0530

    todo

--- js/Todo.js
+++ js/Todo.js
@@ -1,6 +1,5 @@
-let mainTab=[];
+let toDoArray=[];
 function toDo(value) {         //function to get rightpanel of selected button
-  let rightPanelTop = document.getElementsByClassName("right-panel-title");
   let panelSubtask = document.getElementsByClassName("panel-subtask");
   let panelTitle = document.getElementById("panel-title");
   panelTitle.innerHTML = value;
@@ -12,37 +11,138 @@ function toDo(value) {         //function to get rightpanel of selected button
   assignedPanel.style.display = "none";
   lineBox[0].style.display = "flex";
   let title = panelTitle.innerHTML;
+  let openCreatedPanel = document.getElementById("third-panel");
+  openCreatedPanel.style.display= "none";
   var enterValue= document.getElementById("add");
+
   enterValue.onclick=function() {                //function to add and store the subtask value
     let subText = document.getElementById("sub-text");
     let lineArea = document.getElementsByClassName("line");
+    var subTextList = document.createElement("button");
+    subTextList.classList.add("subtask-button");
+    subTextList.innerText= subText.value;
+    let checkBox =document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
     let emptySpace = document.createElement("br");
-    let subTask = subText.value;
-    lineArea[0].append(subTask);
-    lineArea[0].append(emptySpace);
-     subText.value = "";
-    var findingIndex=(mainTab.findIndex(c => c.title==value))
-    if (findingIndex =-1) {
+    let subTask = subTextList.innerText;
+    lineArea[0].appendChild(checkBox);
+    lineArea[0].appendChild(subTextList);
+    lineArea[0].appendChild(emptySpace);
+    subText.value = "";
+
+    checkBox.onclick =function check() {   // function to strike out completed task
+      if(checkBox.checked) {
+        subTextList.style.textDecoration= "line-through";
+        toDoArray[findingIndex].sub[subTaskIndex].strike="true";
+        console.log(toDoArray);
+      }
+      else {
+        subTextList.style.textDecoration= "none";
+        toDoArray[findingIndex].sub[subTaskIndex].strike="false";
+        console.log(toDoArray);
+      }
+    }
+    let storeStrike= "false";
+
+    subTextList.onclick= function () {  //function to show third panel
+      var createdPanelHead = document.getElementById("header");
+      openCreatedPanel.style.display= "flex";
+      createdPanelHead.innerHTML = subTextList.innerText;
+      let addedStepArea = document.getElementById("added-step-area");
+      addedStepArea.innerText="";
+      var addStep= document.getElementById("add-step");
+
+      createdPanelHead.contentEditable="true";
+      createdPanelHead.onkeypress= function(e){    //function for editing subtask
+       if(e.key=="Enter") {
+          let newText = createdPanelHead.innerText;
+          subTextList.innerText= newText;
+          toDoArray[findingIndex].sub[subTaskIndex].head = newText;
+          console.log(toDoArray);
+      }
+      }
+
+      addStep.onclick = function() {  //function to add new step for subtask
+      let newStepBox = document.getElementById("new-step");
+      let addedStepArea = document.getElementById("added-step-area");
+      var newStep = document.createElement("button");
+      newStep.classList.add("subtask-button");
+      newStep.innerText= newStepBox.value;
+      let checkBox =document.createElement("input");
+      checkBox.setAttribute("type", "checkbox");
+      let emptySpace = document.createElement("br");
+      addedStepArea.appendChild(checkBox);
+      addedStepArea.appendChild(newStep);
+      addedStepArea.appendChild(emptySpace);
+      newStepBox.value = ""; 
+      toDoArray[findingIndex].sub[subTaskIndex].step.push({stepHead:newStep.innerText,strike:""});
+      }
+      for (let j=0; j< toDoArray[findingIndex].sub[subTaskIndex].step.length; j++){
+        let addedStepArea = document.getElementById("added-step-area");
+        var newStep = document.createElement("button");
+        newStep.classList.add("subtask-button");
+        let checkBox =document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        let emptySpace = document.createElement("br");
+        newStep.innerText= (toDoArray[findingIndex].sub[subTaskIndex].step[j].stepHead);
+        addedStepArea.appendChild(checkBox);
+        addedStepArea.appendChild(newStep);
+        addedStepArea.appendChild(emptySpace);
+      }
+    }
+    var findingIndex=(toDoArray.findIndex(c => c.title==value))
+    if (findingIndex ==-1) {
       var arrayObject = {title:value, sub:[]};
-      mainTab.push(arrayObject);
-      var findingIndex=(mainTab.findIndex(c => c.title==value))
-      mainTab[findingIndex].sub.push(subTask);
+      toDoArray.push(arrayObject);
+      var findingIndex=(toDoArray.findIndex(c => c.title==value))
+      toDoArray[findingIndex].sub.push({head:subTask, strike:storeStrike,step:[]});
+      var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
     } else {
-      mainTab[findingIndex].sub.push(subTask);
+      toDoArray[findingIndex].sub.push({head:subTask, strike:storeStrike,step:[]});
+      var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
     }
     subText.value = "";
   }
-  var findingIndex=(mainTab.findIndex(c => c.title==value))
-  for(let i = 0; i < mainTab[findingIndex].sub.length; i++) {
-    let enteredList = document.createElement("li");
-    let innerList = document.createTextNode( mainTab[findingIndex].sub[i]);
-    enteredList.style.listStyle= "none";
-    enteredList.appendChild(innerList);
+  var findingIndex=(toDoArray.findIndex(c => c.title==value))
+  for(let i = 0; i < toDoArray[findingIndex].sub.length; i++) {
+    let enteredList = document.createElement("button");
+    enteredList.classList.add("subtask-button");
+    let checkList = document.createElement("input");
+    checkList.setAttribute("type","checkbox");
+    let emptySpace = document.createElement("br");
+    enteredList.innerText =  (toDoArray[findingIndex].sub[i].head);
+    lineArea[0].appendChild(checkList);
     lineArea[0].appendChild(enteredList);
+    lineArea[0].appendChild(emptySpace);
+    if( toDoArray[findingIndex].sub[i].strike == "true") {
+      console.log( toDoArray[findingIndex].sub[i].strike);
+      checkList.checked= true;
+      enteredList.style.textDecoration = "line-through";
+    } else{
+      checkList.checked= false;
+      enteredList.style.textDecoration = "none";
+    }
+    checkList.onclick= function() { // function to strike out completed task
+      if(checkList.checked) {
+        enteredList.style.textDecoration = "line-through";
+        var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+        toDoArray[findingIndex].sub[subTaskIndex].strike="true";
+      }
+      else {
+        enteredList.style.textDecoration = "none";
+        var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+        toDoArray[findingIndex].sub[subTaskIndex].strike="false";
+      }
+    }
+    enteredList.addEventListener('click', function() {  //function to show third panel
+      var createdPanelHead = document.getElementById("header");
+       openCreatedPanel.style.display= "flex";
+       createdPanelHead.innerHTML = enteredList.innerText;
+    });
+
   }
 }
 function assign() {          // funtion for the rightpanel of assigned button
-  let rightPanelTop = document.getElementsByClassName("right-panel-title");
   let panelTitle = document.getElementById("panel-title");
   let panelSubtask = document.getElementsByClassName("panel-subtask");
   let assignedPanel = document.getElementById("assigned-panel");
@@ -52,24 +152,21 @@ function assign() {          // funtion for the rightpanel of assigned button
   assignedPanel.style.display = "block";
   lineBox[0].style.display = "none";
 }
-let array=[];
 function showTask() {         //function to add newtask in the leftpanel
   let input = document.getElementById("new-list");
   let addedTask = document.getElementById("added-task");
   var newList = document.createElement("li");
   newList.classList.add("new-list-style");
   newList.innerText = input.value;
-  var listId= newList.innerHTML;
-  newList.setAttribute("id",listId);
+  var listId= newList.innerText;
+  newList.setAttribute("id",listId)
   addedTask.appendChild(newList);
-  array.push(newList.innerText);
-  console.log(array);
   input.value = "";
   let lineArea = document.getElementsByClassName("line");
   lineArea[0].innerText ="";
   let panelSubtask = document.getElementsByClassName("panel-subtask");
   let panelTitle = document.getElementById("panel-title");
-  let body = newList.innerText;
+  let value = newList.innerText;
   panelTitle.innerHTML = newList.innerText;
   panelSubtask[0].style.display = "block";
   let assignedPanel = document.getElementById("assigned-panel");
@@ -82,7 +179,10 @@ function showTask() {         //function to add newtask in the leftpanel
   deleteButton.innerHTML = "..."
   deleteButton.setAttribute('id','deletor');
   deleteArea.appendChild(deleteButton);
-  newList.addEventListener('click', function() {      //function to get the rightpanel of created newlist
+  let openCreatedPanel = document.getElementById("third-panel");
+  openCreatedPanel.style.display= "none";
+
+  newList.onclick= function() {      //function to get the rightpanel of created newlist
     let panelTitle = document.getElementById("panel-title");
     panelTitle.innerHTML = newList.innerText;
     panelSubtask[0].style.display = "block";
@@ -91,59 +191,112 @@ function showTask() {         //function to add newtask in the leftpanel
     let lineBox = document.getElementsByClassName("lines");
     assignedPanel.style.display = "none";
     lineBox[0].style.display = "flex";
+    openCreatedPanel.style.display= "none";
     var enterValue= document.getElementById("add");
-    enterValue.onclick=function() {                      //function to add and store subtask value of newlist
+    let body = panelTitle.innerText;
+
+    enterValue.onclick=function() {    //function to add and store subtask value of newlist
       let subText = document.getElementById("sub-text");
       let lineArea = document.getElementsByClassName("line");
+      var subTextList = document.createElement("button");
+      subTextList.classList.add("subtask-button");
+      subTextList.innerText= subText.value;
+      let checkBox =document.createElement("input");
+      checkBox.setAttribute("type", "checkBox");
       let emptySpace = document.createElement("br");
-      let subTask = subText.value;
-      lineArea[0].append(subTask);
-      lineArea[0].append(emptySpace);
+      let subTask = subTextList.innerText;
+      lineArea[0].appendChild(checkBox);
+      lineArea[0].appendChild(subTextList);
+      lineArea[0].appendChild(emptySpace);
       subText.value = "";
-      var findingIndex=(mainTab.findIndex(c => c.title==body))
-      if (findingIndex =-1) {
+
+      checkBox.onclick = function() {    // function to strike out completed task
+        if(checkBox.checked){
+        subTextList.style.textDecoration= "line-through";
+        var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+        toDoArray[findingIndex].sub[subTaskIndex].strike="true";
+        }
+        else{
+          subTextList.style.textDecoration= "none";
+          var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+          toDoArray[findingIndex].sub[subTaskIndex].strike="false";
+        }
+      }
+      subTextList.addEventListener('click', function() {  //function to show third panel
+        var createdPanelHead = document.getElementById("header");
+        var openCreatedPanel = document.getElementById("third-panel");
+        openCreatedPanel.style.display= "flex";
+        createdPanelHead.innerHTML = subTextList.innerText;
+      });
+      let storeStrike = "false";
+      var findingIndex=(toDoArray.findIndex(c => c.title==value))
+      if (findingIndex ==-1) {
         var arrayObject = {title:body, sub:[]};
-        mainTab.push(arrayObject);
-        var findingIndex=(mainTab.findIndex(c => c.title==body))
-        mainTab[findingIndex].sub.push(subTask);
+        toDoArray.push(arrayObject);
+        var findingIndex=(toDoArray.findIndex(c => c.title==value))
+        toDoArray[findingIndex].sub.push({head:subTask, strike:storeStrike});
       } else {
-        mainTab[findingIndex].sub.push(subTask);
+        toDoArray[findingIndex].sub.push({head:subTask, strike:storeStrike});
       }
       subText.value = "";
     }
-    let newListValue= newList.innerText
-    var findingIndex=(mainTab.findIndex(c => c.title==body)) 
-    var arrayIndex =(array.findIndex(c => c==newListValue));
-    console.log(arrayIndex);
     panelTitle.contentEditable="true";
-    panelTitle.addEventListener("keypress",function(event){    //function for editing list
+    panelTitle.onkeypress= function(event){    //function for editing list
      if(event.key=="Enter") {
         let newText = panelTitle.innerText;
         var newListId=document.getElementById(listId);
-        newListId.innerText = newText;
-        array[arrayIndex] = newText;
-        mainTab[findingIndex].title = newText;
+        newListId.innerText= newText;
+        toDoArray[findingIndex].title = newText;
     }
-    });
-    var findingIndex=(mainTab.findIndex(c => c.title==body))
-    for(let i = 0; i < mainTab[findingIndex].sub.length; i++) {
-      let enteredList = document.createElement("li");
-      let innerList = document.createTextNode( mainTab[findingIndex].sub[i]);
-      enteredList.style.listStyle= "none";
-      enteredList.appendChild(innerList);
+    }
+    var findingIndex=(toDoArray.findIndex(c => c.title==body))
+    for(let i = 0; i < toDoArray[findingIndex].sub.length; i++) {
+      let enteredList = document.createElement("button");
+      enteredList.classList.add("subtask-button");
+      let checkList = document.createElement("input");
+      checkList.setAttribute("type","checkbox");
+      let emptySpace = document.createElement("br");
+      enteredList.innerText =  (toDoArray[findingIndex].sub[i].head);
+      lineArea[0].appendChild(checkList);
       lineArea[0].appendChild(enteredList);
+      lineArea[0].appendChild(emptySpace);
+      if( toDoArray[findingIndex].sub[i].strike == "true") {
+        checkList.checked= true;
+        enteredList.style.textDecoration = "line-through";
+      } else{
+        checkList.checked= false;
+        enteredList.style.textDecoration = "none";
+      }
+      checkList.onclick= function() { // function to strike out completed task
+        if(checkList.checked) {
+          enteredList.style.textDecoration = "line-through";
+          var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+          toDoArray[findingIndex].sub[subTaskIndex].strike="true";
+        }
+        else {
+          enteredList.style.textDecoration = "none";
+          var subTaskIndex =(toDoArray[findingIndex].sub.findIndex(a => a.head==subTask))
+          toDoArray[findingIndex].sub[subTaskIndex].strike="false";
+        }
+      }
+      enteredList.addEventListener('click', function() {  //function to show third panel
+        var createdPanelHead = document.getElementById("header");
+         openCreatedPanel.style.display= "flex";
+         createdPanelHead.innerHTML = enteredList.innerText;
+      });
     }
-  });
+  }
   newList.addEventListener('dblclick', function(){   //function to delete the created newlist
     addedTask.removeChild(newList);
     panelTitle.innerHTML = "Tasks";
     panelSubtask[0].style.display = "block";
   });
-  deleteButton.addEventListener('click', function(){   //function to delete the created newlist
+  deleteButton.onclick= function() {   //function to delete the created newlist
     let deleteContent = document.getElementById("deleting-button");
     deleteContent.style.display = "flex";
     var deleteList = document.getElementById("confirm-delete");
     deleteList.onclick=function() {
+      //var newListId=document.getElementById(listId);
       addedTask.removeChild(newList);
       //addedTask.removeChild(addedTask.childNodes[0]);
       panelTitle.innerHTML = "Tasks";
@@ -154,7 +307,11 @@ function showTask() {         //function to add newtask in the leftpanel
     cancelor.onclick=function() {
     deleteContent.style.display = "none";
     }
-  });
+  }
+}
+function hideRightPanel() {  //function to hide third panel
+  var openCreatedPanel = document.getElementById("third-panel");
+  openCreatedPanel.style.display= "none";
 }
 function navigate() {       //function to open toggle bar
   var sideBar = document.getElementById("side-tab");
@@ -178,4 +335,3 @@ function closeNavigate() { //function to close toggle bar
       closedBar.style.display ="flex";
   }
 }
-  
